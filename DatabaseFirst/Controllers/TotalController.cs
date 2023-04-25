fae1aa6a03efc537782086e5183b7fbd2887f0a1
DESKTOP-VB4R1O3\Developer <gustavo_hen@hotmail.com>
2023-04-25 20:41:58 -0300

    + Group By on EF

--- DatabaseFirst/Controllers/TotalController.cs
+++ DatabaseFirst/Controllers/TotalController.cs
@@ -9,24 +9,41 @@ namespace DatabaseFirst.Controllers;
 [Route("[controller]")]
 public class TotalController : ControllerBase{
 
+    private readonly PlainContext _context;
+
+    public TotalController(PlainContext context) => _context = context;
+
     [HttpGet("Product")]
-    public async Task<long> GetProductCount([FromServices] PlainContext context)
-        => await context.Products.LongCountAsync();
+    public async Task<long> GetProductCount()
+        => await _context.Products.LongCountAsync();
 
     [HttpGet("Brand")]
-    public async Task<long> GetBrandCount([FromServices] PlainContext context)
-        => await context.Brands.LongCountAsync();
+    public async Task<long> GetBrandCount()
+        => await _context.Brands.LongCountAsync();
 
     [HttpGet("Color")]
-    public async Task<long> GetColorVariantCount([FromServices] PlainContext context)
-        => await context.Variants.LongCountAsync(x => x.VariationType == "Color");
+    public async Task<long> GetColorVariantCount()
+        => await _context.Variants.LongCountAsync(x => x.VariationType == "Color");
 
     [HttpGet("Size")]
-    public async Task<long> GetSizeVariantCount([FromServices] PlainContext context)
-        => await context.Variants.LongCountAsync(x => x.VariationType == "Size");
+    public async Task<long> GetSizeVariantCount()
+        => await _context.Variants.LongCountAsync(x => x.VariationType == "Size");
 
     [HttpGet("Stats")]
-    public async Task<IList<VCharacteristic>> GetStats([FromServices] PlainContext context)
-        => await context.VCharacteristics.ToListAsync();
+    public async Task<IList<VCharacteristic>> GetStats()
+        => await _context.VCharacteristics.ToListAsync();
+
+    [HttpGet("Product-Sizes")]
+    public IActionResult GetProductSizeVariations() 
+        => Ok(
+            (from items in _context.Variants
+                where items.VariationType == "Size"
+                group items by items.Value into temp
+            select new {
+                Total = temp.Count(),
+                Sizes = temp.Key,
+                Type = "Size"
+            }).OrderByDescending(o => o.Total)
+        );
 
 }
