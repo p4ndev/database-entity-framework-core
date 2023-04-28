2b8fe020dd3d6b8cd7b0ae29d828056266bad832
DESKTOP-VB4R1O3\Developer <gustavo_hen@hotmail.com>
2023-04-28 13:01:28 -0300

    + Following branches (subject)

--- CodeFirst/Shared/BaseModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CodeFirst.Shared;
-
-public class BaseModel{
-
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
-
-}
--- CodeFirst/Usings.cs
+++
@@ -1,5 +0,0 @@
-﻿global using System.ComponentModel.DataAnnotations.Schema;
-global using System.ComponentModel.DataAnnotations;
-global using Microsoft.EntityFrameworkCore;
-global using CodeFirst.Shared;
-global using CodeFirst.Models;
